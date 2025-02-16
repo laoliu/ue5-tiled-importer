7fb69ba8b9b3a5f7e706bd3ae957bfdffee0b37b
Joe Scharnitzke <j.scharnitzke@gmail.com>
2025-02-16 10:51:40 -0500

    add basic interchange node for tile sets

---
+++ Source/InterchangeTiled/Private/InterchangeTileSetNode.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "InterchangeTileSetNode.h"
+
+FString UInterchangeTileSetNode::GetTypeName() const
+{
+	const FString TypeName = TEXT("TileSetNode");
+	return TypeName;
+}
---
+++ Source/InterchangeTiled/Public/InterchangeTileSetNode.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Nodes/InterchangeBaseNode.h"
+#include "InterchangeTileSetNode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INTERCHANGETILED_API UInterchangeTileSetNode : public UInterchangeBaseNode
+{
+	GENERATED_BODY()
+	
+public:
+	virtual FString GetTypeName() const override;
+	virtual FString GetAssetName() const override { return AssetName; }
+	
+	virtual bool SetAssetName(const FString& NewAssetName) override 
+	{
+		AssetName = NewAssetName;
+	}
+
+protected:
+
+	UPROPERTY(EditAnywhere)
+	FString AssetName;
+};
