5600a8ebf6a1572845eba6409946908676ebcbec
Joe Scharnitzke <j.scharnitzke@gmail.com>
2025-02-16 11:07:10 -0500

    create basic tile set factory node

---
+++ Source/InterchangeTiled/Public/InterchangeTileSetFactoryNode.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Nodes/InterchangeFactoryBaseNode.h"
+#include "PaperTileSet.h"
+#include "InterchangeTileSetFactoryNode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INTERCHANGETILED_API UInterchangeTileSetFactoryNode : public UInterchangeFactoryBaseNode
+{
+	GENERATED_BODY()
+
+	virtual FString GetTypeName() const override
+	{
+		const FString TypeName = TEXT("TileSetFactoryNode");
+		return TypeName;
+	}
+
+	virtual UClass* GetObjectClass() const override
+	{
+		return UPaperTileSet::StaticClass();
+	}
+};
