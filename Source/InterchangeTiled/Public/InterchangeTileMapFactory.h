bcfddba418f18f38f8a1a4d568f1136d3dbdda67
Joe Scharnitzke <j.scharnitzke@gmail.com>
2025-02-20 10:46:07 -0500

    add tile map factory stubs

---
+++ Source/InterchangeTiled/Public/InterchangeTileMapFactory.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InterchangeFactoryBase.h"
+#include "InterchangeTileMapFactory.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INTERCHANGETILED_API UInterchangeTileMapFactory : public UInterchangeFactoryBase
+{
+	GENERATED_BODY()
+	
+};
