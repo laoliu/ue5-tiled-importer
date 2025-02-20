77ea3c724ea75e84eee2554b2408588e2a351c28
Joe Scharnitzke <j.scharnitzke@gmail.com>
2025-02-20 09:16:28 -0500

    add stubs for TMX translator and node

---
+++ Source/InterchangeTiled/Public/InterchangeTmxTranslator.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InterchangeTranslatorBase.h"
+#include "InterchangeTmxTranslator.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INTERCHANGETILED_API UInterchangeTmxTranslator : public UInterchangeTranslatorBase
+{
+	GENERATED_BODY()
+	
+};
