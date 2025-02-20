1c78f925d5218fa9c338d5bd95ebae2ffe51e084
Joe Scharnitzke <j.scharnitzke@gmail.com>
2025-02-20 11:22:09 -0500

    add tile map synchronous begin asset import logic to factory

--- Source/InterchangeTiled/Private/InterchangeTileMapFactory.cpp
+++ Source/InterchangeTiled/Private/InterchangeTileMapFactory.cpp
@@ -1,5 +1,74 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "InterchangeTileMapFactory.h"
 
+#include "InterchangeSourceData.h"
+#include "InterchangeTiledModule.h"
+
+UInterchangeFactoryBase::FImportAssetResult UInterchangeTileMapFactory::BeginImportAsset_GameThread(const FImportAssetObjectParams& Arguments)
+{
+	TRACE_CPUPROFILER_EVENT_SCOPE(UInterchangeTileMapFactory::BeginImportAsset_GameThread);
+
+	FImportAssetResult ImportAssetResult;
+	UPaperTileMap* TileMap = nullptr;
+
+	if (!Arguments.AssetNode)
+	{
+		LogAssetCreationError(
+			Arguments,
+			LOCTEXT("TileMapFactory_AssetNodeNull", "Asset node parameter is null."),
+			ImportAssetResult
+		);
+
+		return ImportAssetResult;
+	}
+
+	const UClass* TileMapClass = Arguments.AssetNode->GetObjectClass();
+
+	if (!TileMapClass || !TileMapClass->IsChildOf(UPaperTileMap::StaticClass()))
+	{
+		LogAssetCreationError(
+			Arguments,
+			LOCTEXT("TileMapFactory_NodeClassMissmatch", "Asset node parameter class doesn't derive from UPaperTileMap."),
+			ImportAssetResult
+		);
+
+		return ImportAssetResult;
+	}
+
+	UObject* ExistingAsset = Arguments.ReimportObject;
+
+	// even if not flagged as a re-import, try to load the asset in case
+	// it already exists
+	if (!ExistingAsset)
+	{
+		FSoftObjectPath ReferenceObject;
+		if (Arguments.AssetNode->GetCustomReferenceObject(ReferenceObject))
+		{
+			ExistingAsset = ReferenceObject.TryLoad();
+		}
+	}
+
+	if (!ExistingAsset)
+	{
+		TileMap = NewObject<UPaperTileMap>(
+			Arguments.Parent,
+			TileMapClass,
+			*Arguments.AssetName,
+			RF_Public | RF_Standalone
+		);
+	}
+
+	if (!TileMap)
+	{
+		LogAssetCreationError(
+			Arguments,
+			LOCTEXT("TileMapFactory_TileMapCreateFail", "Tile Map creation failed."),
+			ImportAssetResult
+		);
+
+		return ImportAssetResult;
+	}
+
+	ImportAssetResult.ImportedObject = TileMap;
+
+	return ImportAssetResult;
+}
--- Source/InterchangeTiled/Public/InterchangeTileMapFactory.h
+++ Source/InterchangeTiled/Public/InterchangeTileMapFactory.h
@@ -1,17 +1,29 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "InterchangeFactoryBase.h"
+#include "InterchangeTiledFactory.h"
+#include "PaperTileMap.h"
+
 #include "InterchangeTileMapFactory.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class INTERCHANGETILED_API UInterchangeTileMapFactory : public UInterchangeFactoryBase
+class INTERCHANGETILED_API UInterchangeTileMapFactory : public UInterchangeTiledFactory
 {
 	GENERATED_BODY()
-	
+
+
+public:
+
+	virtual UClass* GetFactoryClass() const override
+	{
+		return UPaperTileMap::StaticClass();
+	}
+
+private:
+
+	virtual FImportAssetResult BeginImportAsset_GameThread(const FImportAssetObjectParams& Arguments) override;
 };
